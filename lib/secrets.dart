3b19f1cfd4e6f7c6c7d24873047d3ad242138858
abdullah140404 <smabd7409@gmail.com>
2024-12-08 18:06:11 +0500

    secrets updated

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,1 +1,4 @@
-const openWeatherAPIKey = '2a46cac9fb9af4da8494ab1765afeb12';
+const openWeatherAPIKey = 'YOUR API KEY HERE';
+
+// You can obtain a free API key from https://openweathermap.org/api. Simply visit the website, sign up for an 
+// account, and you'll be able to generate your API key to start integrating weather data into your application.
